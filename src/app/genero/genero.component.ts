10e9f1a17e092027e0e2b049351ad581a963753c
Natan <nt.r.o@hotmail.com>
2019-05-05 15:33:06 -0500

    Genero
    Creación

---
+++ src/app/genero/genero.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-genero',
+  templateUrl: './genero.component.html',
+  styleUrls: ['./genero.component.css']
+})
+export class GeneroComponent implements OnInit {
+
+  constructor() {
+    
+   }
+
+  ngOnInit() {
+  }
+
+
+  
+}
